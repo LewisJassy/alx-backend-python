439b0fe90789bc10196f7c640c5d49c2e1851baa
LewisJassy <jassylewis70@gmail.com>
2024-07-08 21:13:07 +0300

    feat: Add async function wait_random with max_delay parameter
    The code changes add an async function called wait_random that takes in an integer max_delay and returns a random float value with the specified max_delay. This function is implemented in the file 0-basic_async_syntax.py.

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,8 @@
+'''python async function'''
 #!/usr/bin/env python3
 import random
 import asyncio
+'''async function wait_random that takes in an integer max_delay and returns a random float value with the specified max_delay'''
 
 
 async def wait_random(max_delay: int = 10) -> float:
