8568df519cfda633d58b72fb2f00b83846d588f8
LewisJassy <jassylewis70@gmail.com>
2024-07-27 17:36:46 +0300

    mock test for get_json func

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-'''A module for testing the utils module
-'''
+"""A module for testing the utils module
+"""
 
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import Mock, patch
+
 
 '''Test case for `utils.access_nested_map` function.
 '''
@@ -29,6 +31,26 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path, exception):
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        # Arrange
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        # Act
+        result = get_json(test_url)
+
+        # Assert
+        self.assertEqual(result, test_payload)
+        mock_get.assert_called_once_with(test_url)
 
 
 if __name__ == "__main__":
