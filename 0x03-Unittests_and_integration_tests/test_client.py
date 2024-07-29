d438d5a90aa3953dbd01abc8431e7dbc1df1c26b
LewisJassy <jassylewis70@gmail.com>
2024-07-29 11:31:18 +0300

    mock a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """python module"""
 from client import GithubOrgClient
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 
 
@@ -23,6 +23,29 @@ class TestGithubOrgClient(unittest.TestCase):
                 f"https://api.github.com/orgs/{org_name}"
             )
         self.assertEqual(result, {"login": org_name})
+
+    def test_public_repos_url(self):
+        """ Mock payload for the org property"""
+        mock_org_payload = {
+                "repos_url": "https://api.github.com/orgs/test_org/repos"
+            }
+
+        with patch(
+                'client.GithubOrgClient.org', new_callable=PropertyMock
+            ) as mock_org:
+            # Set the return value of the mocked org property
+            mock_org.return_value = mock_org_payload
+
+            client = GithubOrgClient("test_org")
+            result = client._public_repos_url
+
+            # Check that the result is as expected
+            self.assertEqual(
+                    result, "https://api.github.com/orgs/test_org/repos"
+                )
+
+            # Verify that the org property was accessed exactly once
+            mock_org.assert_called_once()
 
 
 if __name__ == "__main__":
