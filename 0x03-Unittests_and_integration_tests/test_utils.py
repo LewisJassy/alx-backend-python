57ef4163322473c3a58ae5d6530aeb18e96d7736
LewisJassy <jassylewis70@gmail.com>
2024-07-29 10:44:24 +0300

    solve the over indentation issue

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -69,10 +69,7 @@ class TestMemoize(unittest.TestCase):
                 ) as mock_method:
             test_instance = TestClass()
 
-            # First call to a_property should call a_method
             result1 = test_instance.a_property
-            # Second call to a_property should return
-            # cached result and not call a_method again
             result2 = test_instance.a_property
 
             self.assertEqual(result1, 42)
